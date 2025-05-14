de639198ab2d54ca0af3cbfecac3820847f60d1d
saengthongdee_r@silpakorn.edu <john.doe@example.com>
2025-05-14 17:50:15 +0700

    finalll

--- src/components/Abouts/about.jsx
+++ src/components/Abouts/about.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React,{useState,useEffect,useRef} from "react";
 import "./about.css";
 
 function about() {
+
+    const containerRef = useRef(null);
+  
+    useEffect(() => {
+      const boxes = containerRef.current.querySelectorAll(".Animation-box");
+  
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add("visible");
+            } else {
+              entry.target.classList.remove("visible");
+            }
+          });
+        },
+        { threshold: 0.3 }
+      );
+  
+      boxes.forEach((box) => observer.observe(box));
+      return () => boxes.forEach((box) => observer.unobserve(box));
+    }, []);
   return (
-    <div>
+    <div ref={containerRef}>
       <div className="about-container">
         <h1>About us</h1>
         <div className="box-text">
-          <div className="image">
+          <div className="image Animation-box slide-image">
             <div className="image-left"></div>
             <div className="image-right"></div>
           </div>
           <div className="box-text-about">
-            <h3>The Gilded Fork</h3>
-            <p>
+            <h3 className="Animation-box slide-text">The Gilded Fork</h3>
+            <p className="Animation-box slide-text">
               At Restaurant, fine dining is not just a meal — it's a celebration
               of taste, elegance, and craftsmanship. We take pride in curating
               an unforgettable culinary experience that combines meticulously
@@ -31,7 +53,7 @@ function about() {
               an unforgettable culinary experience that combines meticulously
               selected ingredients, masterful techniques, and impeccable
             </p>
-            <div className="btn">learn more</div>
+            <div className="btn Animation-box slide-text">learn more</div>
             
           </div>
         </div>
